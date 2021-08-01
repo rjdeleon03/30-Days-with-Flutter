7c6b412a62a4f7a71bc218960116e4349112345a
Ricardo De Leon <rjdeleon@up.edu.ph>
2021-08-01 11:35:35 +0800

    Add update index method

--- day01_restaurant_picker/lib/main.dart
+++ day01_restaurant_picker/lib/main.dart
@@ -12,13 +12,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final restaurants = [
+    "McDonald\'s",
+    "Jollibee",
+    "Pizza Hut",
+    "KFC",
+    "Greenwich"
+  ];
+
+  int currentIndex = 2;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Text("McDonald\'s")
-      )
-    );
+        home: Scaffold(
+            body: Center(
+                child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("What do you want to eat?"),
+        Text(
+          restaurants[currentIndex],
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 50),
+        ),
+        TextButton(onPressed: () {}, child: Text("Pick Restaurant"))
+      ],
+    ))));
   }
-
 }
